4d5b4aa6a5b6f6b91f173c3f813dbab555ebe259
Void&Null <70048414+Void-n-Null@users.noreply.github.com>
2022-06-01 20:53:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import markovify
 with open("updateNotes.txt",'r',encoding="utf8") as f:
     text_model = markovify.Text(f.read())
     
+if __name__ == '__main__':
+    print(text_model.make_short_sentence(150))
